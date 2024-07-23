091f0cc6b2f8dca7721595e7c83ed5ccc6fcdf2c
Jeferson Soares <jpadilha53@gmail.com>
2024-07-23 14:25:23 -0300

    realizado a utilizacao do redux toolkit

---
+++ src/services/api.ts
@@ -0,0 +1,17 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { Game } from '../App'
+
+const api = createApi({
+  baseQuery: fetchBaseQuery({
+    baseUrl: '//localhost:4000'
+  }),
+  endpoints: (builder) => ({
+    getjogos: builder.query<Game[], void>({
+      query: () => 'produtos'
+    })
+  })
+})
+
+export const { useGetjogosQuery } = api
+
+export default api
